2fd9847f45b1885455001208296c7860d5b91223
Jay Gervais <650028@ACDM.DS.SAIT.CA>
2019-06-07 07:49:23 -0600

    Fix error

--- includes/global.php
+++ includes/global.php
@@ -7,11 +7,7 @@ $linksPage = "links.php";
 $agents = "agents.php";
 $packages = "packages.php";
 $addAgent = "addagent.php";
-<<<<<<< HEAD
 $login = "agent-login.php";
-=======
-$login = "customerslogin.php";
->>>>>>> eab928b84fd2003556683f892538430a0b44ff13
 
 // timezone
 date_default_timezone_set("US/Mountain");
